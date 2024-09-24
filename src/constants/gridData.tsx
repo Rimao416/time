daf1dda2adaae29ce5073311b3ad2962cc2409ca
Rimao416 <kayumbaomari27@gmail.com>
2024-09-24 20:21:34 +0200

    Suppression

--- src/constants/gridData.tsx
+++ src/constants/gridData.tsx
@@ -1,32 +1,38 @@
+import Formations_1 from "../assets/formations.jpg";
+import Formations_2 from "../assets/formations_2.jpg";
+import Formations_3 from "../assets/formations_3.jpg";
+import Formations_4 from "../assets/formations_4.jpg";
+import Formations_5 from "../assets/formations_5.jpg";
+import Formations_6 from "../assets/formations_6.jpg";
 export const gridData = [
   {
     id: 1,
-    title: "Executive",
-    image: "https://via.placeholder.com/300",
+    title: "Formations",
+    image: Formations_1,
   },
   {
     id: 2,
-    title: "Health",
-    image: "https://via.placeholder.com/300",
+    title: "Marketing",
+    image: Formations_2
   },
   {
     id: 3,
-    title: "Environment",
-    image: "https://via.placeholder.com/300",
+    title: "Montage",
+    image: Formations_3,
   },
   {
     id: 4,
-    title: "Education",
-    image: "https://via.placeholder.com/300",
+    title: "Ingénierie logicielle",
+    image: Formations_4,
   },
   {
     id: 5,
-    title: "Science",
-    image: "https://via.placeholder.com/300",
+    title: "Certifications",
+    image: Formations_5,
   },
   {
     id: 6,
-    title: "Health",
-    image: "https://via.placeholder.com/300",
+    title: "Systèmes",
+    image: Formations_6,
   }
 ];
